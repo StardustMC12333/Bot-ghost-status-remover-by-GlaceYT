799194ac010d064b17c48455a328474b799ebbbc
StardustMC12333 <support@noedelmc.com>
2024-12-12 19:31:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["play.stardustmc.nl"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["Play.grindhousemc.nl"];
+const statusTypes = [ 'dnd';
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
